29afaca8e7d2c29cbc41e265511b1baff60d8d17
Konstantinos Kontos <kkon@handmadeapps.tech>
2020-04-10 12:42:30 +0300

    Initial Commit

---
+++ Sources/HMDAToolkit/HMDAToolkit.swift
@@ -0,0 +1,3 @@
+struct HMDAToolkit {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import HMDAToolkitTests
+
+var tests = [XCTestCaseEntry]()
+tests += HMDAToolkitTests.allTests()
+XCTMain(tests)
