27c1bb07dc399cca43e478d1f5718dfc24b82761
Konstantinos Kontos <kkon@handmadeapps.tech>
2020-04-12 10:40:03 +0300

    removed amazon s3

--- Package.swift
+++ Package.swift
@@ -17,13 +17,12 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "AWSSDKSwift", url: "https://github.com/swift-aws/aws-sdk-swift.git", from: "4.2.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "HMDAToolkit", dependencies: [
-            .product(name: "S3", package: "AWSSDKSwift")
+            
         ]),
         .testTarget(
             name: "HMDAToolkitTests",
